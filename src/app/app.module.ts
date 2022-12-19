b91a8121713617add558c11f6d1dab0b0eba290d
beemamolshaji <beemamolshaji98@gmail.com>
2022-12-19 21:00:36 +0530

    success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { UserEntryComponent } from './user-entry/user-entry.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { BooksEntryComponent } from './books-entry/books-entry.component';
 
 
 const myRoute:Routes=[
@@ -22,6 +23,10 @@ const myRoute:Routes=[
     path:"user",
     component:UserLoginComponent
   },
+  {
+    path:"booksentry",
+    component:BooksEntryComponent
+  }
  
   
 ]
@@ -31,7 +36,8 @@ const myRoute:Routes=[
     AppComponent,
     UserEntryComponent,
     AdminLoginComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    BooksEntryComponent
   ],
   imports: [
     BrowserModule,
